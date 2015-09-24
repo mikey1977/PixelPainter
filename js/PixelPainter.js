ddd604e96d61c2ad38e33c56e9c12b99be4e6b21
mikey1977 <mmi@umich.edu>
2015-09-23 20:13:22 -1000

    encoding troubleshooting

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -18,13 +18,13 @@ function PixelPainter(width, height) {
   var move = document.createElement('button');
   var encode = document.createElement('button');
   var decode = document.createElement('button');
-  // colorCode = defColors.indexOf(button.style.background); // 2
 
-//save array of colors
+  //save array of colors
   var defColors = ['white', 'silver', 'gray', 'black','red',
                    'maroon', 'yellow', 'olive', 'lime', 'green',
                    'aqua', 'teal', 'blue', 'navy', 'fuchsia', 'purple'];
-//MAKE SWATCH BUTTONS
+
+  //MAKE SWATCH BUTTONS
   erase.className = 'swatchButton';
   clear.className = 'swatchButton';
   encode.className = 'swatchButton';
@@ -46,8 +46,7 @@ function PixelPainter(width, height) {
         buttons[i][j].style.background = 'white';
       }
     }
-
-  })
+  });
   this.topbar.appendChild(erase);
   this.topbar.appendChild(clear);
   this.topbar.appendChild(encode);
@@ -57,27 +56,26 @@ function PixelPainter(width, height) {
 
     //create new buttons for swatch
     swatch[i] = document.createElement('button');
-    swatch[i].className = 'button'
+    swatch[i].className = 'button';
+
     //give background new colors based on i
     swatch[i].style.background = defColors[i];
+
     //add event listener
     swatch[i].addEventListener('click', function() {
       //sets current color to background
       selected = this.style.background;
-    })
+    });
   }
 
-//this.grid BUTTONS AND EVENT LISTENERS
-//ties height to width creating second dimension
+  //this.grid BUTTONS AND EVENT LISTENERS
+  //ties height to width creating second dimension
   for (var i = 0; i < width; i++) {
     buttons[i] = new Array(height);
 
-
-
-//creates buttons
-
-//add event listener to each button
-// handler function takes selected from swatch to change button selected
+    //creates buttons
+    //add event listener to each button
+    // handler function takes selected from swatch to change button selected
     for (var j = 0; j < height; j++) {
       // console.log(buttons[i][j]);
       buttons[i][j] = document.createElement('button');
@@ -85,10 +83,8 @@ function PixelPainter(width, height) {
       buttons[i][j].name = 'button';
       buttons[i][j].addEventListener('click', function() {
 
-
-//this refers to i, j position, set to selected which can be changed
-
-//isPainting is set to false, clicking changes to true
+        //this refers to i, j position, set to selected which can be changed
+        //isPainting is set to false, clicking changes to true
         isPainting = !isPainting;
 
         if (isPainting) {
@@ -96,7 +92,6 @@ function PixelPainter(width, height) {
           if (this.style.background !== 'white') {
 
             index.push(this);
-            // console.log(index);
           }
         }
       });
@@ -114,22 +109,21 @@ function PixelPainter(width, height) {
           }
           lastButton = thisButton;
           thisButton = this;
-          // lastButton = thisButton;  //must set lastButton first
         }
       });
-      // colorCode = defColors.indexOf(button.style.background);
-
     }
   }
 
-//ADD BUTTONS TO DIV this.grid
-//make a div for each row to separate rows
+  //ADD BUTTONS TO DIV this.grid
+  //make a div for each row to separate rows
 
   for (var i = 0; i < width; i++) {
-//making a div within a div
+
+    //making a div within a div
     var temp = document.createElement('div');
 
     for (var j = 0; j < height; j++) {
+
       //append to each div made
       temp.appendChild(buttons[i][j]);
     }
@@ -137,18 +131,20 @@ function PixelPainter(width, height) {
   }
   this.grid.className = 'spacing';
 
-
   //add swatch buttons to HTML this.grid
   //make outside div - border of swatch [  ]
 
   //creates first column [ [...] ]
   var row1 = document.createElement('div');
+
   //creates second column [ [...] [...] ]
   var row2 = document.createElement('div');
 
   for (var i = 0; i < swatch.length; i++) {
+
     //checks between first or second row, if less than swatch.length - first row.
-    if (i < swatch.length/2) {
+    if (i < swatch.length / 2) {
+
       //append here access by i
       row1.appendChild(swatch[i]);
     } else {
@@ -165,31 +161,29 @@ function PixelPainter(width, height) {
   hr.className = 'spacing';
   this.topbar.appendChild(hr);
 
-encode.addEventListener('click', function() {
-  alert(painter.encode());
-})
+  encode.addEventListener('click', function() {
+    window.location.hash = (painter.encode());
+  });
 
-this.encode = function() {
-  var str ='';
-  for (var i = 0; i < index.length; i++) {
-    var colorCode = defColors.indexOf(index[i].style.backgroundColor);
-    var codeId = index[i].dataset.gridIndex;
-    str += '#' + codeId + ':' + colorCode;
-  }
-  // console.log("hello", str);
-  return str;
-}
+  this.encode = function() {
+    var str = '';
+    for (var i = 0; i < index.length; i++) {
+      var colorCode = defColors.indexOf(index[i].style.backgroundColor);
+      var codeId = index[i].dataset.gridIndex;
+      str += '#' + codeId + ':' + colorCode;
+    }
+    return str;
+  };
 
-decode.addEventListener('click', function() {
-  alert(painter.decode());
-})
+  decode.addEventListener('click', function() {
+    window.location.hash = painter.decode();
+  });
 
-this.decode = function() {
-  var j = index % width;
-  var i = parseInt(index/ width);
-  index = (i * width) + j
-
-}
+  this.decode = function() {
+    var j = index % width;
+    var i = parseInt(index / width);
+    index = (i * width) + j;
+  };
 
 }
 PixelPainter.prototype.setup = function() {
@@ -199,12 +193,11 @@ PixelPainter.prototype.setup = function() {
 };
 
 
-
 function popup() {
   alert("STOP SEARCHING FOR PORN!");
   alert("FOR GOD SAKES, YOU'RE IN A PUBLIC ENVIRONMENT!");
   alert("AT LEAST WAIT UNTIL YOU'RE AT HOME!");
-  alert("DON'T EVEN THINK ABOUT SEARCHING FOR YIPPING!")
+  alert("DON'T EVEN THINK ABOUT SEARCHING FOR YIPPING!");
   alert("YOUR SEARCH HISTORY IS BEING SAVED!");
   alert("ALL OF YOUR FACEBOOK FRIENDS ARE BEING NOTIFIED!");
 }
